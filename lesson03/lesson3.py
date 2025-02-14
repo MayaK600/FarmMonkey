1851e65b0485d8498bc2f7d06cc2ac8ad9437133
Maya <Maya.klapauch@gmail.com>
2025-02-14 14:00:38 -0500

    solves lesson 3

--- lesson03/lesson3.py
+++ lesson03/lesson3.py
@@ -1,1 +1,9 @@
 # Lesson 3
+import math
+tiles = input ("Enter the number of input: ") 
+tiles = int(tiles) #Assumes it is string so have to conert to int
+sqrt = math.floor(math.sqrt(tiles))
+print (f"The sides of the square will be {sqrt}")
+# ORR 
+#calculation = int((tiles ** 0.5)//1) --> which then converts it into an int and to be safe using an int f
+# The two // mean floor division, so it divides and rounds down
