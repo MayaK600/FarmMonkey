95df4fdd4a31962f85f8acdefdf838edcbcee326
Maya <Maya.klapauch@gmail.com>
2025-02-18 14:33:44 -0500

    lesson 7 solved

--- lesson07/lesson7.py
+++ lesson07/lesson7.py
@@ -1,1 +1,9 @@
 # Lesson 7
+from random import randrange 
+difficulty = int (input("Enter the DC: "))
+diceRoll= randrange (1,21) #It never includes b
+print (f"The player has rolled:{diceRoll}")
+if diceRoll >=difficulty:
+    print (f"The played was successful as {diceRoll} > {difficulty} ")
+else: 
+    print (f"The player was not successful as {diceRoll} is not greater than {difficulty} ")
