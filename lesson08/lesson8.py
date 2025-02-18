e13ea63f5db5e2e146a9abf2fe3e076c4dc51f50
Maya <Maya.klapauch@gmail.com>
2025-02-18 14:53:54 -0500

    lesson 8 solved

--- lesson08/lesson8.py
+++ lesson08/lesson8.py
@@ -1,1 +1,15 @@
 # Lesson 8
+winCounter=0
+for i in range(6):
+    result = input(f"Enter if you won game number {i+1} ")
+    if result == "W" or result =="w":
+        winCounter+=1
+if winCounter>4:
+    print (f"Player is in group 1")
+elif winCounter >2:
+    print (f"Player is in group 2")
+elif wincounter >0:
+    print (f"Player is in group 3")
+else:
+    print ("Player is eliminated")
+ 
