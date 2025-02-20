524d2a7bb2a06b085f04b2ca3f5a05b1fe58c251
Maya <Maya.klapauch@gmail.com>
2025-02-20 16:47:02 -0500

    lesson 11 completed

--- lesson11/lesson11.py
+++ lesson11/lesson11.py
@@ -1,1 +1,23 @@
 # Lesson 11
+point = input()
+point =point.split(' ')
+
+#fixedPoint = []
+#for value in point:
+#   fixedPoint.append(int(value))
+point = list(map(int, point)) 
+#converts the number in the list into integers 
+#but we cannot use the numbers if it is in map sso we turn it into a list 
+print (point)
+#variable unpacking 
+x , y=point
+if x>0:
+    if y>0:
+        print ("Quadrent 1")
+    else:
+        print ("Quadrent 4")
+else: 
+    if y>0:
+         print("Quadrent 2")
+    else:
+        print ("Quadrent 3")
