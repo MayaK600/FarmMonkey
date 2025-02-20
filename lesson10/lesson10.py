240812886bf7c38eb16341e7906c4a1ee8e06143
Maya <Maya.klapauch@gmail.com>
2025-02-20 16:24:05 -0500

    lesson 10 completed

--- lesson10/lesson10.py
+++ lesson10/lesson10.py
@@ -1,1 +1,13 @@
 # Lesson 10
+firstNum= int(input ("First number: "))
+secondNum= int(input ("Second number: "))
+thirdNum= int(input ("Third number: "))
+fourthNum = int(input ("Fourth number: "))
+safe = True 
+if (firstNum == 9 or firstNum==8) and (fourthNum==9 or fourthNum==8):
+    if (secondNum==thirdNum):
+        safe=False
+if safe:
+    print("Not telemarketer")
+else: 
+    print("telemarketer")
