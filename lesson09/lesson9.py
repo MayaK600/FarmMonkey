8a24cf02eac9f81da47888f9a2c914edd7738a63
Maya <Maya.klapauch@gmail.com>
2025-02-20 16:12:12 -0500

    lesson 9 completed

--- lesson09/lesson9.py
+++ lesson09/lesson9.py
@@ -1,1 +1,28 @@
 # Lesson 9
+user=input("Enter rock paper or scissors: ")
+options = ["rock","paper","scissors"]
+import random
+computer = random.choice(options)
+print (computer)
+if computer == "rock":
+    if user =="paper":
+        print("user wins")
+    elif user== "scissors":
+        print ("computer wins")
+    else:
+        print ("Tie")
+
+elif computer== "scissors":
+    if user=="paper":
+        print ("commputer wins")
+    elif user == "rock":
+        print ("user wins")
+    else :
+        print ("tie")
+else :
+    if user=="rock":
+        print("computer wins")
+    elif user =="scissors":
+        print("user wins")
+    else: 
+        print ("tie")
