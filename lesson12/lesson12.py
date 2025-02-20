c60f5e977dfdbc1770ab0f01ad8c093a448a71f7
Maya <Maya.klapauch@gmail.com>
2025-02-20 17:14:12 -0500

    lesson 12 completed

--- lesson12/lesson12.py
+++ lesson12/lesson12.py
@@ -1,1 +1,14 @@
 # Lesson 12
+angles = input()
+angles = angles.split(' ')
+angles = list(map(int, angles))
+a,b,c=angles
+if sum(angles)==180:
+    if a==b==c:
+        print ("Equilateral")
+    elif a==b or b==c or c==a:
+        print ("Isosceles")
+    else: 
+        print ("Scalene")
+else: 
+    print ("Not triangle")
