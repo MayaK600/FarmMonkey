78e41ba42b5878bd9a9b5870e528c75da8e645e0
Maya <Maya.klapauch@gmail.com>
2025-02-21 10:03:36 -0500

    lesson 24 completed

--- lesson24/lesson24.py
+++ lesson24/lesson24.py
@@ -1,1 +1,12 @@
 # Lesson 24
+name=input("Name: ").lower()
+largestName=''
+length=0
+while name!="x":
+    if len(name)>length:
+        length=len(name)
+        largestName = name
+
+    name=input("Enter next Name: ").lower()
+
+print(f"The longest name is: {largestName}")
