ff5be53904d8d82a7c4d5e3b54c04fc28c8cdb6a
Maya <Maya.klapauch@gmail.com>
2025-02-20 21:04:02 -0500

    lesson 13 completed

--- lesson13/lesson13.py
+++ lesson13/lesson13.py
@@ -1,1 +1,14 @@
 # Lesson 13
+date= int(input("date: "))
+month= int(input("month: "))
+if month>2:
+    print ("After")
+elif month<2:
+    print ("Before")
+else: 
+    if date> 18:
+        print("After")
+    elif date< 18:
+        print ("Before")
+    else:
+        print("Special")
