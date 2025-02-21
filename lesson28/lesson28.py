a05646f64af9e1b67a39e8208080a7de35121548
Maya <Maya.klapauch@gmail.com>
2025-02-21 16:58:50 -0500

    lesson 28 completed

--- lesson28/lesson28.py
+++ lesson28/lesson28.py
@@ -1,1 +1,12 @@
 # Lesson 28
+num=input("Num: ")
+palindrom=True
+i=1
+while i<=len(num):
+    if num[i-1]!=num[i*-1]:
+        palindrom=False 
+    i+=1
+if palindrom: 
+    print("It is a palindrom")
+else:
+    print("It is not a palindrom")
