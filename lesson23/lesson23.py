2e167ff1db46f320a9f25fcca1139f4f1682ee9a
Maya <Maya.klapauch@gmail.com>
2025-02-21 09:17:45 -0500

    lesson 23 completed

--- lesson23/lesson23.py
+++ lesson23/lesson23.py
@@ -1,1 +1,10 @@
 # Lesson 23
+sum=0
+numOfNums=0
+i=float(input("Enter first number (enter -1 to stop): ") )
+while i!=-1:
+    sum+=i
+    numOfNums+=1
+    i=float(input("Enter nextNum: "))
+avg = sum/numOfNums
+print (f"The avg is: {avg}")
