832e02fac39c0af9c79d775295d3c60a08188b50
Maya <Maya.klapauch@gmail.com>
2025-02-21 08:36:46 -0500

    lesson 21 completed

--- lesson21/lesson21.py
+++ lesson21/lesson21.py
@@ -1,1 +1,13 @@
 # Lesson 21
+N=int(input("N: "))
+largestNum=1
+largest=0
+for i in range(1,N):
+    counter =0
+    for p in range(1,i+1):
+         if i%p==0:
+             counter+=1
+    if counter>largest:
+        largestNum=i
+        largest=counter
+print(f"Most amount of factors: {i}")
