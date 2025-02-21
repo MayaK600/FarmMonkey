43401b4efbfb1e28e541156e929ab1275c419e0f
Maya <Maya.klapauch@gmail.com>
2025-02-20 21:21:55 -0500

    lesson 19 completed

--- lesson19/lesson19.py
+++ lesson19/lesson19.py
@@ -1,1 +1,13 @@
 # Lesson 19
+prime =True 
+i = int(input("i: "))
+j=2
+while j<i:
+    if i%j==0:
+        prime=False 
+        break
+    j+=1
+if prime:
+    print(f"{i} is a prime number")
+else:
+    print (f"{i} is not a prime number")
