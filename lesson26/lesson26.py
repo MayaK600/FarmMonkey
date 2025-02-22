3295001a96db5972649590ed92191f7f0453ba1b
Maya <Maya.klapauch@gmail.com>
2025-02-22 10:45:01 -0500

    lesson 26 completed

--- lesson26/lesson26.py
+++ lesson26/lesson26.py
@@ -1,1 +1,17 @@
 # Lesson 26
+def factorCount (number):
+    '''determines the number of factors the argument has 
+    Args:
+        number: an integer needed to determine the number of its factors 
+    Returns: 
+        an integer, which is the total amount of factors the argument has
+    '''
+    for dividor in range (1,number+1):
+        if number % dividor==0:
+            counter+=1
+    return counter
+
+num= int(input("N: "))
+for i in range(1,num+1):
+    factorSize =factorCount(i)
+    print (f"{i} has {factorSize} factors")
