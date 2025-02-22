d175f5dea8e2b99d135d138130a93f951d7debb9
Maya <Maya.klapauch@gmail.com>
2025-02-22 10:31:10 -0500

    lesson 25 completed

--- lesson25/lesson25.py
+++ lesson25/lesson25.py
@@ -1,1 +1,15 @@
 # Lesson 25
+Num=int(input("Num: "))
+largest=0
+while (Num%2==0):
+    Num//=2
+    largest=max(largest,2)
+    print(2)
+for i in range (3,Num+1):
+    while (Num%i==0):
+        print (i)
+        Num//=i
+        largest=max(largest,i)
+    i+=2
+print (f"largest prime: {largest}")
+
