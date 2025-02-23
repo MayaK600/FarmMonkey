af8653583641b82fbda736895f70452b639620ec
Maya <Maya.klapauch@gmail.com>
2025-02-23 17:39:13 -0500

    lesson 32 completed

--- lesson32/lesson32.py
+++ lesson32/lesson32.py
@@ -1,1 +1,19 @@
 # Lesson 32
+def characterFinder(text,text2):
+    '''Prints all characters used from strings
+    input: 
+    Text1 is a string based input that consist of different characters
+    Output: 
+    Putputs all the character that the string has without repetition
+    '''
+    text=text.replace(" ","")
+    text2=text2.replace(" ","")
+    totalCharacters=''
+    for character in text:
+        if character not in totalCharacters and character in text2:
+            totalCharacters+=character 
+    return sorted(totalCharacters)
+
+input1=characterFinder(input("Word 1: "), input("Word 2: "))
+
+print(f"The characters are: {input1}")
