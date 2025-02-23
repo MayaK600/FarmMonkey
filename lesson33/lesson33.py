0a941ed4ab55829d3d9b076ca193dad1b6e49a27
Maya <Maya.klapauch@gmail.com>
2025-02-23 18:17:03 -0500

    lesson 33 completed

--- lesson33/lesson33.py
+++ lesson33/lesson33.py
@@ -1,1 +1,14 @@
 # Lesson 33
+def medianFinder(numberList):
+    numberList=numberList.split(" ")
+    numberList=list(map(float, numberList))
+    numberList=sorted(numberList)
+    median=0
+    if len(numberList)%2==0:
+        median =(numberList[len(numberList)//2 -1] + (numberList[(len(numberList)//2)*(-1)]))/2
+    else: 
+        median = numberList[len(numberList)//2]
+    mean=sum(numberList)/len(numberList)
+    return median,mean
+
+print (medianFinder("3 7 1 2 5"))
