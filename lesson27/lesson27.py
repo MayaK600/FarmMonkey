29b59843783741e5c15a2165cf00dc1a7a2e57db
Maya <Maya.klapauch@gmail.com>
2025-02-23 08:29:00 -0500

    lesson 27 completed

--- lesson27/lesson27.py
+++ lesson27/lesson27.py
@@ -1,1 +1,13 @@
 # Lesson 27
+def stringCleaner(text):
+    '''cleans string and retuns the word with only alpha characters in lower case
+    input: User inputs a certain word 
+    output: The program outputs all alpha characters in lower case together
+    '''
+    reslut =""
+    for character in text:
+        if character.isalpha():
+            result+=character.lower()
+    return result 
+value= stringCleaner("Hell0, world!")
+print (value)
