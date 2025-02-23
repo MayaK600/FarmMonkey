a04b5bd37f4b6afe0490ce8b2fa5cc81bac38266
Maya <Maya.klapauch@gmail.com>
2025-02-23 16:17:02 -0500

    lesson 30 completed

--- lesson30/lesson30.py
+++ lesson30/lesson30.py
@@ -1,1 +1,9 @@
 # Lesson 30
+num = int(input("Num: "))
+pyramid=''
+for number in range (1,num+1):
+    if number % 2 ==1:
+        pyramid+='1'
+    else:
+        pyramid+='0'
+    print (pyramid)
