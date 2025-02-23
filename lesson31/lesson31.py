229086df1295d0b7e36c1a6e26f04369c47606ef
Maya <Maya.klapauch@gmail.com>
2025-02-23 16:47:33 -0500

    lesson 31 completed

--- lesson31/lesson31.py
+++ lesson31/lesson31.py
@@ -1,1 +1,16 @@
 # Lesson 31
+def anagramChecker(text1,text2):
+    '''Checks if two strings are anagrams of each other 
+    Input: 
+        text1= first string  input 
+        text2= a second string input 
+    Output: Outputs True or False based on if it is an anagram or not
+    '''
+    checker=True
+    for character in text1:
+        if character.isalpha() and character in text2:
+            text2=text2.replace (character,"",1) 
+        else :
+            checker=False 
+    return checker
+print (f"The words are anagrams: {anagramChecker('hello','ollhe')}")
