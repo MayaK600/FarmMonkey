e650997ce87939d6ea4ba8fce2c908dfae2357e0
Maya <Maya.klapauch@gmail.com>
2025-02-24 12:07:47 -0500

    lesson 23 completed

--- lesson23/lesson23.py
+++ lesson23/lesson23.py
@@ -1,10 +1,16 @@
 # Lesson 23
+loop =True
 sum=0
 numOfNums=0
-i=float(input("Enter first number (enter -1 to stop): ") )
-while i!=-1:
-    sum+=i
-    numOfNums+=1
+i=input("Enter first number (enter x to stop): ").lower()
+while loop:
+    if i=='x':
+        loop = False
+        break
+    else: 
+        i=float(i)
+         sum+=i
+         numOfNums+=1
     i=float(input("Enter nextNum: "))
 avg = sum/numOfNums
 print (f"The avg is: {avg}")
