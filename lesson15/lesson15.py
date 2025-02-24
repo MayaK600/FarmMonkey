290ae1ac11e311408d9b6d333d9d6e79cb562dba
Maya <Maya.klapauch@gmail.com>
2025-02-24 12:43:31 -0500

    lesson 15 completed

--- lesson15/lesson15.py
+++ lesson15/lesson15.py
@@ -1,1 +1,10 @@
 # Lesson 15
+userInput=input("Enter fruit: ").lower()
+fruits= ['apple','bananas', 'kiwi', 'strawberry']
+found= False
+for fruit in fruits: 
+    if fruit == userInput:
+        print (f"{userInput} is found!")
+        found = True 
+if not found: 
+    print (f"{userInput} is not found")
