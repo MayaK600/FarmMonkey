83de2cb100d7ac41873116de99e31864761f9bff
Maya <Maya.klapauch@gmail.com>
2025-03-04 14:36:51 -0500

    lesson 28 re-completed

--- lesson28/lesson28.py
+++ lesson28/lesson28.py
@@ -1,12 +1,15 @@
 # Lesson 28
-num=input("Num: ")
-palindrom=True
-i=1
-while i<=len(num):
-    if num[i-1]!=num[i*-1]:
-        palindrom=False 
-    i+=1
-if palindrom: 
-    print("It is a palindrom")
+def palindromCheck(num): 
+    palindrom=True
+    i=1
+    while i<=len(num):
+         if num[i-1]!=num[i*-1]:
+             palindrom=False 
+         i+=1   
+    return palindrom 
+
+number=palindromCheck((input("Enter number: ")))
+if number:
+    print("it is a palindrom")
 else:
-    print("It is not a palindrom")
+    print ("Not a palindrom")
