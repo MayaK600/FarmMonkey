348545b888ee4109061e5867c4f8cad35b66d3d9
Maya <Maya.klapauch@gmail.com>
2025-03-05 14:37:16 -0500

    lesson 35 completed

--- lesson35/lesson35.py
+++ lesson35/lesson35.py
@@ -1,1 +1,17 @@
 # Lesson 35
+def doubleRemover(listA):
+    ''' Function will look over a list of integers and create a new list with those values 
+    and have no double occur
+    '''
+    listA.split(",")
+    listB=[]
+    if len(listA)>1:
+        for i in range(len(listA)):
+            currentCharacter=listA[i]
+            if currentCharacter not in listB:
+                listB.append(currentCharacter)
+    else:
+        listB=listA
+    return listB
+    
+print(doubleRemover("4,n,7,n,4,5"))
