364927bf6cdbad84449dbc160da76425910a8c65
Maya <Maya.klapauch@gmail.com>
2025-03-06 14:24:30 -0500

    lesson 40 completed

--- lesson40/lesson40.py
+++ lesson40/lesson40.py
@@ -1,1 +1,17 @@
 # Lesson 40
+def primeCheck(number):
+    counter=0
+    for i in range(2,number):
+        if number%i==0:
+            counter+=1
+    return counter==0
+
+def primeNumList (N):
+    primes=[]
+    for i in range(2,N):
+        if primeCheck(i):
+            primes.append(i)
+    return primes
+
+print(primeNumList(int(input("Enter number: "))))
+
