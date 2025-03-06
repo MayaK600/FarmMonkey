c7f292e75e4156455d32ffa0da6ff184dad5788c
Maya <Maya.klapauch@gmail.com>
2025-03-06 13:32:37 -0500

    lesson 39 completed

--- lesson39/lesson39.py
+++ lesson39/lesson39.py
@@ -1,1 +1,11 @@
 # Lesson 39
+def devisorChecker(num,div):
+    return num%div==0
+
+num=int(input("Enter number: "))
+largestDiv=[]
+for div in range(1,num):
+    if devisorChecker(num,div):
+        largestDiv.append(div)
+
+print(f"largest divisor is {max(largestDiv)}")
