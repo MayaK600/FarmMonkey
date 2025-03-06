a3a7d65a894922e7808d3a5ac007f4832d342155
Maya <Maya.klapauch@gmail.com>
2025-03-06 13:12:21 -0500

    lesson 38 completed

--- lesson38/lesson38.py
+++ lesson38/lesson38.py
@@ -1,1 +1,18 @@
 # Lesson 38
+def palindromeCheck(number):
+    
+    return number==number[::-1]
+
+num1=999
+num2=999
+palindrome=[]
+while num2>99: 
+    while num1>99:
+        currentNum=num1*num2
+        if palindromeCheck(str(currentNum)):
+             palindrome.append(currentNum)
+        num1-=1
+    num1=num2-1
+    num2-=1
+
+print(f"largest palindrome is {max(palindrome)}")
