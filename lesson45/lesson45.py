d93e2c983ab31cef40fc21a767b1c37744dd1fa8
Maya <Maya.klapauch@gmail.com>
2025-03-19 18:36:41 -0400

    lesson 45 completed

--- lesson45/lesson45.py
+++ lesson45/lesson45.py
@@ -1,1 +1,9 @@
 # Lesson 45
+def wordLen(aList):
+    dictionary={}
+    for word in aList:
+        dictionary[word]=len(word)
+    return dictionary 
+
+test=["apple","cherry","banana","lemon"]
+print(wordLen(test))
