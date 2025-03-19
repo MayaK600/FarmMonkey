a3201ba660bd26aba2a2e5f81479f28ad25439ed
Maya <Maya.klapauch@gmail.com>
2025-03-19 18:43:56 -0400

    lesson 45 completed

--- lesson45/lesson45.py
+++ lesson45/lesson45.py
@@ -7,3 +7,15 @@ def wordLen(aList):
 
 test=["apple","cherry","banana","lemon"]
 print(wordLen(test))
+
+#Fibonacci:
+def d_fib(num):
+    result={0:0 , 1:1}
+    if num in result: 
+        return result 
+    else:
+        for i in range(2,num+1):
+            result[i]=result[i-1]+result[i-2]
+    return result 
+
+print(d_fib(4))
