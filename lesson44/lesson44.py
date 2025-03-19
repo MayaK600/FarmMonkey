f2d5392e4e16c0e7ca1684513b107983d7a416aa
Maya <Maya.klapauch@gmail.com>
2025-03-19 14:51:01 -0400

    lesson 44 completed

--- lesson44/lesson44.py
+++ lesson44/lesson44.py
@@ -1,1 +1,13 @@
 # Lesson 44
+def cFrequency(word):
+    characterList=sorted(word.lower())
+    answer = {}
+    for letter in characterList:
+        if letter not in answer:
+            answer[letter]=1
+        else:
+            answer[letter]+=1     
+    return answer
+
+test="hello"
+print (cFrequency(test))
