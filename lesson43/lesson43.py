f85dca6b7591c2cdd6e1c6f6bcf7625e4a15931a
Maya <Maya.klapauch@gmail.com>
2025-03-19 13:54:34 -0400

    lesson 43 completed

--- lesson43/lesson43.py
+++ lesson43/lesson43.py
@@ -1,1 +1,10 @@
 # Lesson 43
+def duplicateRemove(aList):
+    ''' turns a list into a set so the duplicates are removed'''
+    aSet=set()
+    for item in aList:
+        aSet.add(item)
+    return aSet
+
+test=[1,2,3,3,45,5,45]
+print (duplicateRemove(test))
