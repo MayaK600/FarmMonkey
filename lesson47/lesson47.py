d713aa944f953b2eb5ed6eeea3ac73f2ae78bd81
Maya <Maya.klapauch@gmail.com>
2025-03-20 13:29:33 -0400

    lesson 47 completed

--- lesson47/lesson47.py
+++ lesson47/lesson47.py
@@ -1,1 +1,9 @@
-# Lesson 47
+# Lesson 4
+def sumFinder(num):
+    if num ==0 or num ==1:
+        return num
+    else: 
+        return num +sumFinder(num-1)
+
+print (f"sum of numbers between 1 to 10 is: {sumFinder(10)}")
+    
